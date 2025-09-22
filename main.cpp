205bbe96b61e312bdf0515fa1b3cf94bcc1d44ca
smslsz <2138799740lsz@gmail.com>
2025-09-22 15:58:11 +0800

    model

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(){ 
+    int n, m;
+    cin>>n>>m;//物品数与背包容量
+    int v[n],w[n];//物品价值和重量
+    int dp[n][m];//在前n个物品中以m为容量的背包能获得的最高价值
+    //有此状态转移方程 
+    //if(w[i]>j) dp[i][j]=dp[i-1][j] 不能放入
+    //if(w[i]<=j]) dp[i][j]=max(dp[i-1][j], dp[i-1][j-w[i]]+v[i])
+    //用双重循环m,n即可
+
+    //也有一维dp滚动用法
+    //如果仅用dp[m]记录，则必须逆序遍历
+    //这是因为：
+    //此时状态转移方程为
+    //if(w[i]>j) dp[j]=dp[j];
+    //if(w[i]<=j) dp[j]=max(dp[j], dp[j-w[i]+v[i]])
+    //在调用"dp[j-w[i]]"时，这个值实际上已经被更新过了，因为我们是
+    //从小到大遍历的，j-w[i]这个index先于j更新
+    //如此会导致此时的dp[j-w[i]]已经不是上一行(即i-1时)的数据，可能会出错
+    //因此应用for(int i=0;i<=n;i++){
+    //  for(int j=m;j>0;j--)
+    //}遍历
+}
